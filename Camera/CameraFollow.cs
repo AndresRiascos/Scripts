903320d24aaf301a763c914c853456f886466ffa
AndresRiascos <carlos.a.riascos@correounivalle.edu.co>
2016-06-06 07:30:02 -0500

    Cámara
    Dos Vistas con escalación

--- Camera/CameraFollow.cs
+++ Camera/CameraFollow.cs
@@ -7,9 +7,17 @@ namespace CompleteProject
     {
         public Transform target;            // The position that that camera will be following.
         public float smoothing = 5f;        // The speed with which the camera will be following.
-		public float zoomZ = 0.002f;
-
-
+		public int zoomMaxPerspective= 60;
+		public int zoomMinPerspective = 15;
+		public int zoomMaxOrthographic= 15;
+		public int zoomMinOrthographic = 5;
+		//PARA EL CAMBIO DE CAMARA
+		public bool orthoOn = false;
+		public Camera MainCamera;
+		public Camera Camera2;
+		public GameObject CamPerspective;
+		public GameObject CamOrthographic;
+		//FIN CAMBIO CAMARA
         Vector3 offset;                     // The initial offset from the target.
 
 
@@ -17,40 +25,76 @@ namespace CompleteProject
         {
             // Calculate the initial offset.
             offset = transform.position - target.position;
+			//CAMARA
+			CamPerspective = GameObject.Find("MainCamera");
+			MainCamera = CamPerspective.GetComponent<Camera>();
+			MainCamera.enabled = true;
+			CamOrthographic = GameObject.Find ("Camera2");
+			Camera2 = CamOrthographic.GetComponent<Camera>();
+			Camera2.enabled = false;		
+			//
         }
 
 
         void FixedUpdate ()
         {
+			if ( Input.GetKeyDown(KeyCode.E) ){
+				orthoOn = !orthoOn;
+				if (orthoOn) {
+					MainCamera.enabled = false;
+					Camera2.enabled = true;				
+				} else {
+					MainCamera.enabled = true;
+					Camera2.enabled = false;
+				}
+			}if (( Input.GetKeyDown(KeyCode.KeypadPlus)) && (MainCamera.enabled == true)){
+				Debug.Log (MainCamera.transform.localEulerAngles.x);
+				Debug.Log (MainCamera.transform.localPosition);
+				int comparar = (int)MainCamera.transform.localEulerAngles.x;
+				if(comparar == 30){
+					var rotation = Quaternion.Euler(45, 0, 0);
+					Vector3 temp = MainCamera.transform.localPosition;
+					temp.z = -14f;
+					MainCamera.transform.localPosition = temp;
+					MainCamera.transform.rotation = rotation;
+				}if(comparar == 45){
+					var rotation = Quaternion.Euler(60, 0, 0);
+					MainCamera.transform.rotation = rotation;
+				}if(comparar == 60){
+					var rotation = Quaternion.Euler(75, 0, 0);
+					MainCamera.transform.rotation = rotation;
+				}if (comparar == 74) {
+					var rotation = Quaternion.Euler (90, 0, 0);
+					MainCamera.transform.rotation = rotation;
+				}if (comparar == 90) {
+					MainCamera.transform.rotation = MainCamera.transform.rotation;
+				}
 
-			// Mouse wheel moving forwards
-			if(Input.GetAxis("Mouse ScrollWheel") > 0)
-			{
-				Vector3 Zoom = new Vector3 (0, 10,0);//para hacer el zoom pero creo que no sirve
-				//target.localScale += Zoom; //con esto escalo solamente el muñeco pero no el ambiente
-				Vector3 targetCamPos = target.position + offset ;
-				// Smoothly interpolate between the camera's current position and it's target position.
-				//Vector3 temp = transform.localPosition;
-				//temp.x += 0.1f;
-				//temp.y += 10f;
-				//temp.z += 0.1f;
-				//transform.localPosition = temp;
-				Camera.main.fieldOfView = Camera.main.fieldOfView - 5;
-				transform.position = Vector3.Lerp (transform.position , targetCamPos , smoothing * Time.deltaTime);
+				//Debug.Log (MainCamera.transform.localRotation);
 			}
-
+			// Mouse wheel moving forwards
+			if((Input.GetAxis("Mouse ScrollWheel") > 0) && (MainCamera.fieldOfView > zoomMinPerspective) && (MainCamera.enabled == true)){
+				MainCamera.fieldOfView = MainCamera.fieldOfView - 5;
+			}
 			// Mouse wheel moving backwards
-			if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
-				Vector3 targetCamPos = target.position + offset;
-				// Smoothly interpolate between the camera's current position and it's target position.
-				Camera.main.fieldOfView = Camera.main.fieldOfView + 5;
-				transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
-			} else {
+			if((Input.GetAxis ("Mouse ScrollWheel") < 0) && (MainCamera.fieldOfView < zoomMaxPerspective) && (MainCamera.enabled == true)) {
+				MainCamera.fieldOfView = MainCamera.fieldOfView + 5;
+			}
+			if((Input.GetAxis("Mouse ScrollWheel") > 0) && (Camera2.orthographicSize > zoomMinOrthographic) && (Camera2.enabled == true)){
+				Camera2.orthographicSize = Camera2.orthographicSize - 1;
+			}
+			// Mouse wheel moving backwards
+			if((Input.GetAxis ("Mouse ScrollWheel") < 0) && (Camera2.orthographicSize < zoomMaxOrthographic) && (Camera2.enabled == true)) {
+				Camera2.orthographicSize = Camera2.orthographicSize + 1;
+			}else{
 				// Create a postion the camera is aiming for based on the offset from the target.
 				Vector3 targetCamPos = target.position + offset;
 				// Smoothly interpolate between the camera's current position and it's target position.
 				transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 			}
         }
+			
+
+
     }
 }
